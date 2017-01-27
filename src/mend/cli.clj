2d3d5bf5a9772a11cb85709e8f9f855682cf09e9
Joel Martin <github@martintribe.org>
2017-01-26 19:41:11 -0600

    Compile CSS grammar to generators. CSS parse fixes

--- src/mend/cli.clj
+++ src/mend/cli.clj
@@ -141,60 +141,121 @@
 
 ;; (def tx (parsed-tree->map (css3-parser (slurp "./css-syntax/transition.pvs"))))
 
-(declare component component-multiplied component-single components
-         double-amp single-pipe double-pipe adjacent)
+(declare single-pipe adjacent components double-amp double-pipe
+         component component-single component-multiplied brackets
+         braces)
+
+(defn single-pipe
+  "One of the values must occur."
+  [tree indent]
+  (let [pre (apply str (repeat indent "  "))]
+    (if (= 1 (count tree))
+      (adjacent (drop 1 (first tree)) indent)
+      (str pre "(gen/frequency [\n"
+           (clojure.string/join
+             "\n"
+             (for [t tree]
+               (str pre "  [100\n" (adjacent (drop 1 t) (+ 2 indent)) "]")))
+           "])"))))
+
+(defn adjacent
+  "Each value must occur."
+  [tree indent]
+  (let [pre (apply str (repeat indent "  "))]
+    (if (= 1 (count tree))
+      (components (second (first tree)) indent)
+      (str pre "(gen/tuple\n"
+           (clojure.string/join
+             "\n"
+             (for [t tree]
+               (if (= :comma (first t))
+                 (str pre "  (gen/return \",\")")
+                 (components (second t) (+ 1 indent)))))
+           ")"))))
+
+(defn components [tree indent]
+  (condp = (first tree)
+    :double-amp  (double-amp  (drop 1 tree) indent)
+    :double-pipe (double-pipe (drop 1 tree) indent)))
+
+(defn double-amp
+  "All values must occur in any order."
+  [tree indent]
+  (let [pre (apply str (repeat indent "  "))]
+    (if (= 1 (count tree))
+      (component (second (first tree)) indent)
+      (str pre "(gen/let [lst (gen/tuple \n"
+           (clojure.string/join
+             "\n"
+             (for [t tree]
+               (component (second t) (+ 8 indent))))
+           ")]\n"
+           pre "  (shuffle lst))"))))
+
+(defn double-pipe
+  "One or more of the values must occur in any order."
+  [tree indent]
+  (let [pre (apply str (repeat indent "  "))]
+    (if (= 1 (count tree))
+      (component (second (first tree)) indent)
+      (str pre "(gen/let [cnt (gen/choose 1 " (count tree) ")\n"
+           pre "          lst (gen/tuple \n"
+           (clojure.string/join
+             "\n"
+             (for [t tree]
+               (component (second t) (+ 8 indent))))
+           ")]\n"
+           pre "  (take cnt (shuffle lst)))"))))
 
 (defn component [tree indent]
   (condp = (first tree)
     :component-single     (component-single (second tree) indent)
-    :component-multiplied (component-multiplied (drop 1 tree) indent)
-    :components           (components (second tree) indent)))
+    :component-multiplied (component-multiplied (drop 1 tree) indent)))
 
 (defn component-single [tree indent]
   (let [pre (apply str (repeat indent "  "))]
     (condp = (first tree)
       :literal       (str pre "(gen/return \"" (second tree) "\")")
       :keyword-value (str pre "(gen/return \"" (second tree) "\")")
-      :non-property  (str pre "(gen/return gen-nonprop-" (second tree) ")")
-      :property      (str pre "(gen/return gen-prop-" (second tree) ")")
-      :brackets      (component (second tree) indent))))
+      :non-property  (str pre "gen-nonprop-" (second tree))
+      :property      (str pre "gen-prop-" (second tree))
+      :brackets      (brackets (drop 1 tree) indent))))
 
 (defn component-multiplied [[tree multiplier] indent]
-  (let [pre (apply str (repeat indent "  "))]
-    (str pre "GEN/multiplier")))
+  (let [pre (apply str (repeat indent "  "))
+        single (component-single (second tree) (+ 1 indent))]
+    (condp = (first (second multiplier))
+      :question    (str pre "(gen/one-of [(gen/return \"\")\n" single "])")
+      :asterisk    (str pre "(gen/vector\n" single ")")
+      :plus        (str pre "(gen/such-that not-empty (gen/vector\n" single "))")
+      :hash        (str pre "(gen/fmap #(interpose \" , \" %) (gen/such-that not-empty (gen/vector\n" single ")))")
+      :braces      (braces (second (second multiplier)) single indent)
+      :hash-braces (str pre "(gen/fmap #(interpose \" , \" %)\n"
+                        (braces (second (second (second multiplier)))
+                                (component-single (second tree) (+ 2 indent))
+                                (+ 1 indent))")"))))
 
-(defn components [tree indent]
-  (condp = (first tree)
-    :double-amp  (double-amp  (drop 1 tree) indent)
-    :single-pipe (single-pipe (drop 1 tree) indent)
-    :double-pipe (double-pipe (drop 1 tree) indent)
-    :adjacent    (adjacent    (drop 1 tree) indent)))
+(defn brackets [tree indent]
+  ;; TODO: deal with bang?
+  (single-pipe (drop 1 (first tree)) indent))
 
-(defn double-amp [tree indent]
-  (str (apply str (repeat indent "  "))
-       "GEN/double-amp"))
-
-(defn single-pipe [tree indent]
-  (let [pre (apply str (repeat indent "  "))]
-    (str pre "(gen/frequency [\n"
-         (clojure.string/join
-           "\n"
-           (for [t tree]
-             (str pre "  [100\n" (component t (+ 2 indent)) "]")))
-         "])")))
-
-(defn double-pipe [tree indent]
-  (str (apply str (repeat indent "  "))
-       "GEN/double-pipe"))
-
-(defn adjacent [tree indent]
-  (let [pre (apply str (repeat indent "  "))]
-    (str pre "(gen/tuple\n"
-         (clojure.string/join
-           "\n"
-           (for [t tree]
-             (component t (+ 1 indent))))
-         ")")))
+(defn braces [kind single indent]
+  (prn :here1 kind single)
+  (let [pre (apply str (repeat indent "  "))
+        bmin (apply str (drop 1 (second kind)))
+        bmax (apply str (drop 1 (nth kind 2 nil)))]
+    (prn :here2 bmin bmax)
+    (condp = (first kind)
+      :bracesA   (str pre "(gen/vector \n"
+                      single "\n"
+                      pre "  " bmin ")")
+      :bracesA-B (str pre "(gen/vector \n"
+                      single "\n"
+                      pre "  " bmin " " bmax ")")
+      ;; TODO: fix this 20 value
+      :bracesA-  (str pre "(gen/vector \n"
+                      single "\n"
+                      pre "  " bmin " 20)"))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -203,8 +264,43 @@
     (str "gen-prop-" (clojure.string/replace k #"'" ""))
     (str "gen-nonprop-" k)))
 
+(defn prefix [ns]
+  (str
+"(ns " ns "
+   (:require [clojure.test.check.generators :as gen]))\n"
+"
+(def gen-nonprop-integer gen/int)
+
+(def gen-nonprop-length gen/pos-int)
+
+(def gen-nonprop-color (gen/return \"red\"))
+
+(defn flatten-text* [tree]
+  (lazy-seq
+    (cond
+      (= \"\" tree)
+      (list)
+
+      (or (number? tree) (string? tree))
+      (list tree \" \")
+
+      :else
+      (mapcat flatten-text* tree))))
+
+(defn flatten-text [tree]
+  (clojure.string/trimr
+    (clojure.string/replace
+      (apply str (flatten-text* tree))
+      #\" +\" \" \")))
+
+"))
+
 (defn map->generators [m]
   (apply str
          (for [[k v] m]
            (str "(def " (generator-name k) "\n"
-                (component v 1) ")\n\n"))))
+                "  (gen/fmap flatten-text\n"
+                (single-pipe (drop 1 v) 2) "))\n\n"))))
+
+(defn map->ns [ns m]
+  (str (prefix ns) (map->generators m)))
