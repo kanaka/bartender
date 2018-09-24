317d6fb18ddb233398d3c32c127bbc323b3a205e
Joel Martin <github@martintribe.org>
2018-09-24 16:23:46 -0500

    Cleanup split between config and state.
    - Make check-state local to -main and pass the atom to the check-page
      and reporter functions.
    - Move id and test-dir values into config since they do not change
      over a test run. Only things left in state variable are the index
      (test iteration), the log entries and the browser webdriver session
      info.

--- src/rend/cli.clj
+++ src/rend/cli.clj
@@ -18,10 +18,10 @@
             [clojure.java.io :as io]
             [clojure.java.shell :refer [sh]]))
 
-(def check-state (atom {}))
-
-(defn check-page* [cfg test-dir test-index html]
-  (let [text (hiccup.core/html html)
+(defn check-page* [cfg state test-index html]
+  (let [test-dir (:test-dir cfg)
+        sessions (:sessions @state)
+        text (hiccup.core/html html)
         test-prefix (str test-dir "/" test-index)
         path (str test-prefix ".html")
         url (str (webdriver/addr (:web cfg)) "/" path)]
@@ -36,8 +36,8 @@
 
       ;; Load the page in each browser
       (println "Loading" url "in each browser")
-      (doseq [browser (:browsers cfg)]
-        (webdriver/load-page browser url))
+      (doseq [[browser session] sessions]
+        (webdriver/load-page session url))
 
       (Thread/sleep 1000)
 
@@ -57,9 +57,9 @@
             comp-fn (fn [x] (comp-op threshold x))
             d-path (str test-prefix "_diffs.edn")
             images (into {}
-                         (for [browser (:browsers cfg)]
+                         (for [[browser session] sessions]
                            (let [ss-path (str test-prefix "_" (:id browser)  ".png")
-                                 img (webdriver/screenshot-page browser ss-path)]
+                                 img (webdriver/screenshot-page session ss-path)]
                              [browser img])))
             imgs (apply assoc {}
                         (interleave (keys images)
@@ -123,28 +123,31 @@
         (prn :check-page-exception t)
         [false "Exception" nil]))))
 
-;; SIDE-EFFECTS: updates :index in check-state atom
-(defn new-test-index []
-  (let [s (swap! check-state update-in [:index] #(+ 1 %))]
+;; SIDE-EFFECTS: updates :index in state atom
+(defn new-test-index [state]
+  (let [s (swap! state update-in [:index] #(+ 1 %))]
     (:index s)))
 
-;; SIDE-EFFECTS: updates :log in check-state atom
+;; SIDE-EFFECTS: updates :log in state atom, updates the overall
+;; report index.html, and broadcasts the updates to any browsers
+;; viewing the report document.
 (defn check-page
   "Render and check a test case page. Then output a report file and
   send an update to any browsers currently viewing the page."
-  [cfg test-dir html]
-  (let [test-index (new-test-index)
-        [res diffs violations] (check-page* cfg test-dir test-index html)
+  [cfg state html]
+  (let [test-dir (:test-dir cfg)
+        test-index (new-test-index state)
+        [res diffs violations] (check-page* cfg state test-index html)
         log-entry {:prefix (str test-dir "/" test-index)
                    :html html
                    :result res
                    :diffs diffs
                    :violations violations}
-        state (swap! check-state update-in [:log] conj log-entry)]
+        state-val (swap! state update-in [:log] conj log-entry)]
     ;; Generate report page after every check and notify browsers
     ;; viewing it of the change.
     (spit (str test-dir "/index.html")
-          (rend.html/render-report cfg state))
+          (rend.html/render-report cfg state-val))
     (rend.server/ws-broadcast
       (str
         "row:"
@@ -156,21 +159,21 @@
       (str
         "summary:"
         (hiccup.core/html
-          (rend.html/render-summary state))))
+          (rend.html/render-summary state-val))))
     res))
 
 (defn reporter
   "Prune clojure objects from the report data and print it. If the
   current report type has changed output a report file and send an
   update to any browsers currently viewing the page."
-  [cfg test-dir report]
+  [cfg state report]
   (let [r (dissoc report :property)
 	r (update-in r [:current-smallest]
 		     dissoc :function)]
     (when (:verbose cfg)
       (prn :report (merge r {:failing-args :elided :args :elided})))
     ;; Save the latest report
-    (swap! check-state
+    (swap! state
            (fn [s]
              (merge s {:latest-report r}
                     (when (:trial-number r)
@@ -215,48 +218,61 @@
 (defn -main [& argv]
   (let [{:keys [options arguments]} (opt-errors (parse-opts argv cli-options
                                                             :summary-fn usage))
-        cfg-file (first arguments)
-        cfg (deep-merge (yaml/parse-string (slurp cfg-file))
-                        {:verbose (:verbose options)}
+        cfg-path (first arguments)
+        file-cfg (yaml/parse-string (slurp cfg-path))
+        test-id (rand-int 100000)
+        test-dir (str (-> file-cfg :web :dir) "/" test-id "-"
+                      (-> file-cfg :quick-check :seed))
+        cfg (deep-merge file-cfg
+                        {:test-id test-id
+                         :test-dir test-dir
+                         :verbose (:verbose options)}
                         (if (:seed options)
                           {:quick-check {:seed (:seed options)}}))
         _ (do (println "Configuration:") (pprint cfg))
         weights (when (:weights cfg)
                   (edn/read-string (slurp (:weights cfg))))
-        id (rand-int 100000)
 
-        test-dir (str (-> cfg :web :dir) "/" id "-" (-> cfg :quick-check :seed))
         _ (println "Test case/web service dir: " test-dir)
         _ (io/make-parents (java.io.File. (str test-dir "/index.html")))
+        ;; Page tracking state
+        check-state (atom {:index 0
+                           :log []
+                           :sessions {}})
+        ;; Config and state specific versions of the
+        ;; functions/generators used for the actual check process
+        check-fn (partial check-page cfg check-state)
+        reporter-fn (partial reporter cfg check-state)
+        gen-html-fn (rend.generator/get-html-generator weights)
+        ;; Cleanup browser sessions on exit
+        cleanup-fn (fn []
+                     (println "Cleaning up browser sessions")
+                     (doseq [[browser session] (:sessions @check-state)]
+                       (println "Stopping browser session for:" (:id browser))
+                       (try
+                         (webdriver/stop-session session)
+                         (swap! check-state update-in [:sessions] dissoc browser)
+                         (catch Throwable e
+                           (println "Failed to stop browser session:" e)))))
         ;; Start a web server for the test cases and reporting
-        server (rend.server/start-server cfg (str test-dir "/"))
-        ;; Set the initial page tracking state
-        _ (reset! check-state {:index 0
-                               :id id
-                               :test-dir test-dir
-                               :log []})
-        ;; Create the initial empty page
-        _ (spit (str test-dir "/index.html")
-                (rend.html/render-report cfg @check-state))
-        ;; Create webdriver/selenium sessions to the testing browsers
-        browsers (for [browser (:browsers cfg)]
-                   (do
-                     (println "Initializing browser session for:" (:id browser))
-                     (webdriver/init-session browser)))
-        cleanup-fn (fn [] (doseq [browser browsers]
-                            (println "Stopping browser session for:" (:id browser))
-                            (prn-str (webdriver/stop-session browser))))
-        ;; Update config with session driver enriched browsers
-        cfg (assoc cfg :browsers browsers)]
+        server (rend.server/start-server cfg (str test-dir "/"))]
+
+    ;; Create the initial empty page
+    (spit (str test-dir "/index.html")
+          (rend.html/render-report cfg @check-state))
+
+    ;; Create webdriver/selenium sessions to the testing browsers
+    (doseq [browser (:browsers cfg)]
+      (println "Initializing browser session for:" (:id browser))
+      (swap! check-state update-in [:sessions]
+             assoc browser (webdriver/init-session browser)))
+
 
     ;; Run the tests and report
-    (let [gen-html (rend.generator/get-html-generator weights)
-          start-time (t/now)
-          check-fn (partial check-page cfg test-dir)
-          reporter-fn (partial reporter cfg test-dir)
+    (let [start-time (t/now)
           qc-res (instacheck/run-check
                    (:quick-check cfg {})
-                   gen-html
+                   gen-html-fn
                    check-fn
                    reporter-fn)
           end-time (t/now)
